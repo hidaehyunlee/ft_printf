298ed943fd369d62973d67ac1a73c8ab9eab270d
leehi0210 <leehi0210@gmail.com>
2020-05-26 19:28:06 +0900

    Add parse_format

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,70 @@
 /*   By: daelee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/29 20:23:54 by daelee            #+#    #+#             */
-/*   Updated: 2020/03/29 21:37:21 by daelee           ###   ########.fr       */
+/*   Updated: 2020/05/26 19:26:48 by daelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int					print_type(va_list ap, t_info *info)
+{
+	int				ret;
+
+	ret = 0;
+	if (info->type == 'c')
+		ret = print_c(ap, info);
+	return (ret);
+}
+
+void				init_info(t_info *info)
+{
+	info->minus = 0;
+	info->zero = 0;
+	info->dot = 0;
+	info->width = 0;
+	info->prec = 0;
+	info->type = 0;
+}
+
+int					parse_format(va_list ap, char *format)
+{
+	int				i;
+	int				ret;
+	static	t_info	*info;
+
+	i = 0;
+	ret = 0;
+	if (!(info = (t_info)malloc(sizeof(t_info) * 1)))
+		return (NULL);
+	while (format[i] != '\0')
+	{
+		while (format[i] != '%' && format[i] != '\0')
+		{
+			ft_putchar(format[i], 1);
+			ret++;
+			i++;
+		}
+		if (format[i] == '%')
+		{
+			i++;
+			init_info(info);
+			while (format[i] %% !(ft_strchr(TYPE, format[i])))
+				check_info(ap, format, info, i); //flag, width, prec 체크
+			if(ft_strchr(TYPE, format[i]))
+				info->type = format[i++];
+			ret += print_type(ap, info);
+		}
+	}
+	free(info);
+	return (ret);
+}
+
+int					ft_printf(const char *format, ...)
+{
+	int				ret;
+	va_list			ap;
+
+	va_start(ap, format);
+	ret = parse_format(ap, (char *)format);
+	va_end(ap);
+	return (ret);
+}
--- ft_printf.h
+++ ft_printf.h
@@ -6,15 +6,28 @@
 /*   By: daelee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/29 20:23:51 by daelee            #+#    #+#             */
-/*   Updated: 2020/05/26 17:56:19 by daelee           ###   ########.fr       */
+/*   Updated: 2020/05/26 18:56:50 by daelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include "./libft/libft.h"
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "./libft/libft.h"
 
+# define TYPE "csdiupxX"
+
+typedef struct	s_info
+{
+	int			minus;
+	int			zero;
+	int			dot;
+	int			width;
+	int			prec;
+	char		type;
+}				t_info
+
+int				ft_printf(const char *format, ...);
