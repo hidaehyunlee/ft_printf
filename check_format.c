d492578fcc016a597ecff14f938c2fd84793f899
leehi0210 <leehi0210@gmail.com>
2020-05-30 20:49:33 +0900

    Add check_flag

--- check_format.c
+++ check_format.c
@@ -6,7 +6,7 @@
 /*   By: daelee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/27 21:19:24 by daelee            #+#    #+#             */
-/*   Updated: 2020/05/27 21:51:50 by daelee           ###   ########.fr       */
+/*   Updated: 2020/05/28 18:24:44 by daelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 void	check_flag(char *format, t_list info, int i)
 {
-
+	if (format[i] == '-')
+		info->minus = 1;
+	else if (format[i] == '0')
+		info->zero = 1;
 }
 
 void	check_width(va_list ap, char *format, t_list info, int i)
